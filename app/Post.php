48035b66aa0fa75809d0dc349fbaf7030e4e9bcc
tricci <tricci@MOB11630.hq.k.grp>
2015-03-31 15:56:01 +0200

    Updated Views

--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model {
 
-	//
+	protected $fillable=["title","body","highlighted"];
+  
+  public function user(){
+    return $this->belongsTo('User');
+  }
 
 }
--- app/Project.php
+++ app/Project.php
@@ -4,6 +4,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model {
 
-	//
+	protected $fillable=["title","teaser","body","highlighted"];
+  
+  public function user(){
+    return $this->belongsTo('User');
+  }
 
+  public function generateSlug(){
+    $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+    $clean = preg_replace("/[^a-zA-Z0-9\/_| -]/", '', $clean);
+    $clean = strtolower(trim($clean, '-'));
+    $clean = preg_replace("/[\/_| -]+/", '-', $clean);
+    return $clean;
+  }
 }
